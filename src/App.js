631945bc5af901c3ed01d88fa2b2f94e55e01335
Thomas <th.baier@posteo.de>
2020-03-30 12:27:10 +0200

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -45,17 +45,32 @@ class App extends React.Component { // convert to
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUpPage}/>
+        <Route exact path='/signin' render={() => 
+          this.props.currentUser ? (
+            <Redirect to ='/' />
+            ): (
+              <SignInAndSignUpPage/>
+            )
+            }
+          />
      </Switch>
     </div>
   );
   }
 }
+// gives access to this.state.currentUser
+const mapStateToProps = ({user}) =>({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+  )(App);
 // make dynamik urls by using <Link to={`${props.match.url}/13`}></Link>
 // which calls the RoutsPath and adds something on top.
 // Switch: Makes sure only to render the route it fits first
