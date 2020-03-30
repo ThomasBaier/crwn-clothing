6bbf18409a2e256075dc5f7a46a862a9769b2670
Thomas <th.baier@posteo.de>
2020-03-30 15:16:56 +0200

    added cartIcon, cartDropdow, cartReducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,13 +5,17 @@ import {Link } from 'react-router-dom';
 import {ReactComponent as Logo } from './../../assets/crwn.svg'
 import './header.styles.scss'
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
+
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
         </Link>
+
         <div className='options'>
             <Link className='option' to='/shop'>
                 SHOP
@@ -27,11 +31,17 @@ const Header = ({ currentUser }) => (
                 SIGN IN
             </Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
     </div>
 )
-const mapStateToProps = state =>({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: {hidden} }) =>({ //destructure nested values
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
